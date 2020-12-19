eed14e4263e9a8a0225f280c1544e22afc2ef181
Emmanuel Ajuelos <60262354+ajuelosemmanuel@users.noreply.github.com>
2020-12-19 16:39:00 +0100

    Add TCP options

--- tcp.ts
+++ tcp.ts
@@ -1,3 +1,98 @@
+const lookupTCPOptions = [
+        "End of options list",
+        "No operation",
+        "Maximum Segment Size",
+        "WSOPT - Window Scale",
+        "SACK Permitted",
+        "SACK (Selective ACK)",
+        "Echo",
+        "Echo Reply",
+        "TSOPT - Time Stamp Option",
+        "Partial Order Connection Permitted",
+        "Partial Order Service Profile",
+        "CC",
+        "CC.NEW",
+        "CC.ECHO",
+        "TCP Alternate Checksum Request",
+        "TCP Alternate Checksum Data"
+];
+
+const parseOptions = ( bytes: number[] ): { [ key: string ]: string }[] => {
+
+    if ( bytes.length === 0 )
+
+        return [];
+
+    if ( bytes[ 0 ] === 0 )
+
+        return [ { Kind: "0x00 (End of options list)" } ];
+
+    if ( bytes[ 0 ] === 1 )
+
+        return [ { Kind: "0x01 (No operation)" }, {}, ...parseOptions( bytes.slice( 1 ) ) ];
+
+    if ( bytes[ 0 ] === 2 )
+
+        return [ {
+
+            Kind: "0x02 (Maximum Segment Size)",
+            Length: "0x04 (4)",
+            "MSS Value": "0x" + bytes.slice( 2, 4 ).map( dec => dec.toString( 16 ).padStart( 2, "0" ) ).join( "" ) + " (" + parseInt(bytes.slice( 2, 4 ).map( dec => dec.toString( 16 ).padStart( 2, "0" ) ).join( "" ), 16) + ")"
+
+        },{} , ...parseOptions( bytes.slice( 4 ) ) ];
+
+    if ( bytes[ 0 ] === 3 )
+
+        return [ {
+
+            Kind: "0x03 (WSOPT - Window Scale)",
+            Length: "0x03 (3)",
+            "Shift count": "0x" + bytes[3].toString( 16 ).padStart( 2, "0" ) + " (" + bytes[3] + ")"
+
+        }, {}, ...parseOptions( bytes.slice( 3 ) ) ];
+
+
+    if ( bytes[ 0 ] === 4 )
+
+        return [ {
+
+            Kind: "0x04 (Selective Acknowledgement permitted)",
+            Length: "0x02 (2)"
+
+        }, {}, ...parseOptions( bytes.slice( 2 ) ) ];
+
+    if ( bytes[ 0 ] === 5 )
+
+        return [ {
+
+            Kind: "0x05 (SACK - Selective ACK)",
+            Length: "0x" + bytes[ 1 ].toString( 16 ).padStart( 2, "0" ) + " (" + bytes[ 1 ] + ")",
+            "left edge": "0x" + bytes.slice( 2, ( 1 + (bytes[1])/2 ) ).map( dec => dec.toString( 16 ).padStart( 2, "0" ) ).join( "" ),
+            "right edge": "0x" + bytes.slice( 1 + (bytes[1])/2, bytes[ 1 ] ).map( dec => dec.toString( 16 ).padStart( 2, "0" ) ).join( "" )
+
+        }, {}, ...parseOptions( bytes.slice( bytes[ 1 ] ) ) ];
+
+    if ( bytes[ 0 ] === 8 )
+
+        return [ {
+
+            Kind: "0x08 (TSOPT - Time Stamp Option)",
+            Length: "0x0A (10)",
+            "Timestamp value": "0x" + bytes.slice( 2, 6 ).map( dec => dec.toString( 16 ).padStart( 2, "0" ) ).join( "" ) + " (" + parseInt(bytes.slice( 2, 6 ).map( dec => dec.toString( 16 ).padStart( 2, "0" ) ).join( "" ), 16) + ")",
+            "Timestamp echo reply": "0x" + bytes.slice( 6, 10 ).map( dec => dec.toString( 16 ).padStart( 2, "0" ) ).join( "" ) + " (" + parseInt(bytes.slice( 6, 10 ).map( dec => dec.toString( 16 ).padStart( 2, "0" ) ).join( "" ), 16) + ")"
+
+        }, {}, ...parseOptions( bytes.slice( 10 ) ) ];
+
+    return [ {
+
+        Kind: "0x" + bytes[ 0 ].toString( 16 ).padStart( 2, "0" ) + " (" + lookupTCPOptions[ bytes[ 0 ] ] + ")",
+        Length: "0x" + bytes[ 1 ].toString( 16 ).padStart( 2, "0" ) + " (" + bytes[ 1 ] + ")",
+        Data: "0x" + bytes.slice( 2, bytes[ 1 ] ).map( dec => dec.toString( 16 ).padStart( 2, "0" ) ).join( "" )
+
+    }, {}, ...parseOptions( bytes.slice( bytes[ 1 ] ) ) ];
+
+};
+
 export const layerToTCP = ( layer: number[] ) => {
 
     const hexLayer = layer.map( byte => byte.toString( 16 ) );
@@ -33,6 +128,8 @@ export const layerToTCP = ( layer: number[] ) => {
 
     const urgentPointer = "0x" + (hexLayer[18] + hexLayer[19]) + " (" + parseInt((hexLayer[18] + hexLayer[19]), 16) + ")";
 
+    const optionsBytes = layer.slice( 20 );
+
     return {
         'Source Port': sourcePort,
         'Destination Port': destinationPort,
@@ -42,6 +139,7 @@ export const layerToTCP = ( layer: number[] ) => {
         ['Flags ('+'0x'+ parseInt((hexLayer[12][1] + hexLayer[13]), 16) +')']: flags,
         'Window size value': windowSize,
         'Checksum': checkSum,
-        'Urgent pointer': urgentPointer
+        'Urgent pointer': urgentPointer,
+        'Options': parseOptions( optionsBytes )
     };
 }
